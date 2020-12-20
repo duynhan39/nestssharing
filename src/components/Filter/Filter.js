4c91de027a01226a636eca77c41a4871fc3d1642
Nhan <caotrongduynhan@gmail.com>
2020-12-20 16:03:37 -0600

    Filter options in a FlatList

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,19 +1,41 @@
 import React from 'react'
-import { Text, View, StyleSheet, SafeAreaView, ScrollView } from 'react-native'
+import { Text, View, StyleSheet, SafeAreaView, FlatList } from 'react-native'
 
 import styles from './styles'
 
+function FilterOptionBox(props) {
+    const option = props.option
+
+    return (
+        <Text>?</Text>
+    );
+
+
+}
+
 export default function Filter(props) {
+
+    const options = [
+        {"id": "1", "name": "Books"},
+        {"id": "2", "name": "Tools"},
+        {"id": "3", "name": "Skills"},
+    ]
   
     const listing = props.listing
+
+    const renderItem = ({item}) => (
+        <FilterOptionBox option={item}/>
+    );
   
     return (
       <View>
-          <ScrollView horizontal={true}>
+          <FlatList 
+            // horizontal={true}
+            data={options}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+            />
 
-            <Text>Filter</Text>
-            <Text>Filter</Text>
-          </ScrollView>
       </View>
     );
   }
